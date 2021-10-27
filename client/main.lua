2b6287b86ee37dda68070cda30a047292c3bd79e
BombayV <OMGmsebastian@gmail.com>
2021-10-26 23:32:14 -0400

    minor cleanup

--- client/main.lua
+++ client/main.lua
@@ -7,7 +7,7 @@ Script = {
     Zones = { ['golf'] = "Los Santos Golf Club", ['AIRP'] = "Los Santos International Airport", ['ALAMO'] = "Alamo Sea", ['ALTA'] = "Alta", ['ARMYB'] = "Fort Zancudo", ['BANHAMC'] = "Banham Canyon Dr", ['BANNING'] = "Banning", ['BEACH'] = "Vespucci Beach", ['BHAMCA'] = "Banham Canyon", ['BRADP'] = "Braddock Pass", ['BRADT'] = "Braddock Tunnel", ['BURTON'] = "Burton", ['CALAFB'] = "Calafia Bridge", ['CANNY'] = "Raton Canyon", ['CCREAK'] = "Cassidy Creek", ['CHAMH'] = "Chamberlain Hills", ['CHIL'] = "Vinewood Hills", ['CHU'] = "Chumash", ['CMSW'] = "Chiliad Mountain State Wilderness", ['CYPRE'] = "Cypress Flats", ['DAVIS'] = "Davis", ['DELBE'] = "Del Perro Beach", ['DELPE'] = "Del Perro", ['DELSOL'] = "La Puerta", ['DESRT'] = "Grand Senora Desert", ['DOWNT'] = "Downtown", ['DTVINE'] = "Downtown Vinewood", ['EAST_V'] = "East Vinewood", ['EBURO'] = "El Burro Heights", ['ELGORL'] = "El Gordo Lighthouse", ['ELYSIAN'] = "Elysian Island", ['GALFISH'] = "Galilee", ['GOLF'] = "GWC and Golfing Society", ['GRAPES'] = "Grapeseed", ['GREATC'] = "Great Chaparral", ['HARMO'] = "Harmony", ['HAWICK'] = "Hawick", ['HORS'] = "Vinewood Racetrack", ['HUMLAB'] = "Humane Labs and Research", ['JAIL'] = "Bolingbroke Penitentiary", ['KOREAT'] = "Little Seoul", ['LACT'] = "Land Act Reservoir", ['LAGO'] = "Lago Zancudo", ['LDAM'] = "Land Act Dam", ['LEGSQU'] = "Legion Square", ['LMESA'] = "La Mesa", ['LOSPUER'] = "La Puerta", ['MIRR'] = "Mirror Park", ['MORN'] = "Morningwood", ['MOVIE'] = "Richards Majestic", ['MTCHIL'] = "Mount Chiliad", ['MTGORDO'] = "Mount Gordo", ['MTJOSE'] = "Mount Josiah", ['MURRI'] = "Murrieta Heights", ['NCHU'] = "North Chumash", ['NOOSE'] = "N.O.O.S.E", ['OCEANA'] = "Pacific Ocean", ['PALCOV'] = "Paleto Cove", ['PALETO'] = "Paleto Bay", ['PALFOR'] = "Paleto Forest", ['PALHIGH'] = "Palomino Highlands", ['PALMPOW'] = "Palmer-Taylor Power Station", ['PBLUFF'] = "Pacific Bluffs", ['PBOX'] = "Pillbox Hill", ['PROCOB'] = "Procopio Beach", ['RANCHO'] = "Rancho", ['RGLEN'] = "Richman Glen", ['RICHM'] = "Richman", ['ROCKF'] = "Rockford Hills", ['RTRAK'] = "Redwood Lights Track", ['SANAND'] = "San Andreas", ['SANCHIA'] = "San Chianski Mountain Range", ['SANDY'] = "Sandy Shores", ['SKID'] = "Mission Row", ['SLAB'] = "Stab City", ['STAD'] = "Maze Bank Arena", ['STRAW'] = "Strawberry", ['TATAMO'] = "Tataviam Mountains", ['TERMINA'] = "Terminal", ['TEXTI'] = "Textile City", ['TONGVAH'] = "Tongva Hills", ['TONGVAV'] = "Tongva Valley", ['VCANA'] = "Vespucci Canals", ['VESP'] = "Vespucci", ['VINE'] = "Vinewood", ['WINDF'] = "Ron Alternates Wind Farm", ['WVINE'] = "West Vinewood", ['ZANCUDO'] = "Zancudo River", ['ZP_ORT'] = "Port of South Los Santos", ['ZQ_UAR'] = "Davis Quartz" },
     Speeds = {
         ['kmh'] = 3.6,
-        ['mph'] = 2.236936
+        ['mph'] = 2.23693629
     }
 }
 PLAYER = {
@@ -53,7 +53,7 @@ CreateThread(function()
         if PLAYER.Loaded then
             local Player = PlayerPedId()
             local PlayerCoords = GetEntityCoords(Player)
-            local street1, street2 = GetStreetNameAtCoord(PlayerCoords.x, PlayerCoords.y, PlayerCoords.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
+            local _, street2 = GetStreetNameAtCoord(PlayerCoords.x, PlayerCoords.y, PlayerCoords.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
             local PlayerVehicle = GetVehiclePedIsIn(Player)
             if esxState then
                 local PD = ESX.GetPlayerData()
@@ -141,7 +141,7 @@ end
 function getPlayerStatus()
     if esxState then
         AddEventHandler('esx_status:onTick', function(status)
-            for i,v in pairs(status) do
+            for _, v in pairs(status) do
                 if v.name == 'hunger' then 
                     PLAYER.Hunger = v.percent
                 elseif v.name == 'thirst' then 
@@ -165,7 +165,6 @@ function getVehicleData(Player, PlayerVehicle)
         local speed = math.floor((GetEntitySpeed(PlayerVehicle) * Script.Speeds[Config.SpeedUnit]) + 0.5)
         local gear = GetVehicleCurrentGear(PlayerVehicle)
         local fuel = math.floor(GetVehicleFuelLevel(PlayerVehicle))
-        
         return {speed = speed, gear = gear, fuel = fuel}
     else
         return {speed = 0, gear = 0, fuel = 0}
